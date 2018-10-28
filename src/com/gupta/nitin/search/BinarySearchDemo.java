dca02b7a9680a7a803a9d510855216021527a6d0
nitin <nitinguptamca@gmail.com>
2018-10-29 01:06:00 +0530

    done

--- src/com/gupta/nitin/search/BinarySearchDemo.java
+++ src/com/gupta/nitin/search/BinarySearchDemo.java
@@ -1,6 +1,30 @@
 package com.gupta.nitin.search;
 
-//All element is sorted order.
+/**
+ * A binary search or half-interval search algorithm finds the position of a
+ * specified value (the input "key") within a sorted array. In each step, the
+ * algorithm compares the input key value with the key value of the middle
+ * element of the array. If the keys match, then a matching element has been
+ * found so its index, or position, is returned. Otherwise, if the sought key is
+ * less than the middle element's key, then the algorithm repeats its action on
+ * the sub-array to the left of the middle element or, if the input key is
+ * greater, on the sub-array to the right. If the remaining array to be searched
+ * is reduced to zero, then the key cannot be found in the array and a special
+ * "Not found" indication is returned.
+ * 
+ * Every iteration eliminates half of the remaining possibilities. This makes
+ * binary searches very efficient - even for large collections.
+ * 
+ * Binary search requires a sorted collection. Also, binary searching can only
+ * be applied to a collection that allows random access (indexing).
+ * 
+ * Worst case performance: O(log n)
+ * 
+ * Best case performance: O(1)
+ * 
+ * @author nitin
+ *
+ */
 public class BinarySearchDemo {
 
 	public static int recursiveBinarySearch(int[] sortedArray, int start, int end, int key) {
@@ -50,7 +74,7 @@ public class BinarySearchDemo {
 		System.out.println("Found 45 at " + index + " index");
 		index = recursiveBinarySearch(arr1, 0, arr1.length, 999);
 		System.out.println("Found 999 at " + index + " index");
-		index = recursiveBinarySearch(arr1, 0, arr1.length, 876);
+		index = recursiveBinarySearch(arr1, 0, arr1.length, 890);
 		System.out.println("Found 876 at " + index + " index");
 
 	}
