b58123602e7795b3dadb51a43c9ebd8959acea90
nitin <nitinguptamca@gmail.com>
2018-10-29 00:19:31 +0530

    update files.

--- src/com/gupta/nitin/revench/LinearSearch.java
+++ src/com/gupta/nitin/revench/LinearSearch.java
@@ -2,8 +2,24 @@ package com.gupta.nitin.revench;
 
 public class LinearSearch {
 
+	private static int linerSearch(int[] arr, int searchKey) {
+		int size = arr.length;
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == searchKey) {
+				return i;
+			}
+		}
+		return -1;
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr1 = { 23, 45, 21, 55, 234, 1, 34, 90 };
+		int searchKey = 34;
+		System.out.println("Key " + searchKey + " found at index: " + linerSearch(arr1, searchKey));
+		int[] arr2 = { 123, 445, 421, 595, 2134, 41, 304, 190 };
+		searchKey = 421;
+		System.out.println("Key " + searchKey + " found at index: " + linerSearch(arr2, searchKey));
 
 	}
 
