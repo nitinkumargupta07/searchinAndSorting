6f7baae1e1ee0c88c1cc3808f41017b4ca1e753e
nitin <nitinguptamca@gmail.com>
2018-10-29 23:33:09 +0530

    update quick sort.

--- src/com/gupta/nitin/sort/MyBubbleSort.java
+++ src/com/gupta/nitin/sort/MyBubbleSort.java
@@ -1,10 +1,88 @@
 package com.gupta.nitin.sort;
 
-public class MyBubbleSort  {
+public class MyBubbleSort {
+	static int[] arr = { 12, 22, 42, 6, 34, 76, 23, 98, 45 };
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/// bubbleSort(arr);
+		/// selectonSort(arr);
+		/// insertionSort(arr);
+		quickSort(arr ,0, arr.length - 1);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+	/** Quick sort function **/
+	public static void quickSort(int arr[], int low, int high) {
+		int i = low, j = high;
+		int pivot = arr[(low + high) / 2];
+
+		/** partition **/
+		while (i <= j) {
+			while (arr[i] < pivot)
+				i++;
+			while (arr[j] > pivot)
+				j--;
+			if (i <= j) {
+				/** swap **/
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+
+				i++;
+				j--;
+			}
+		}
+
+		/** recursively sort lower half **/
+		if (low < j)
+			quickSort(arr, low, j);
+		/** recursively sort upper half **/
+		if (i < high)
+			quickSort(arr, i, high);
+	}
+
+	public static void insertionSort(int[] input) {
+		int temp;
+		for (int i = 1; i < input.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (input[j] < input[j - 1]) {
+					temp = input[j];
+					input[j] = input[j - 1];
+					input[j - 1] = temp;
+				}
+			}
+		}
+	}
+
+	static void selectonSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			// Find the minimum element in unsorted arr
+			int min_idx = i;
+			for (int j = i + 1; j < n; j++)
+				if (arr[j] < arr[min_idx])
+					min_idx = j;
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	static void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					// swap temp and arr[i]
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
 	}
 
 }
