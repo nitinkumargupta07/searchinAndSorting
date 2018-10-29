67c559863eec2c420e0168ae035ef5e8c39b719e
nitin <nitinguptamca@gmail.com>
2018-10-30 00:02:21 +0530

    aa

--- src/com/gupta/nitin/sort/MyBubbleSort.java
+++ src/com/gupta/nitin/sort/MyBubbleSort.java
@@ -2,18 +2,68 @@ package com.gupta.nitin.sort;
 
 public class MyBubbleSort {
 	static int[] arr = { 12, 22, 42, 6, 34, 76, 23, 98, 45 };
+	static private int[] tempMergArr;
 
 	public static void main(String[] args) {
 
 		/// bubbleSort(arr);
 		/// selectonSort(arr);
 		/// insertionSort(arr);
-		quickSort(arr ,0, arr.length - 1);
+		/// quickSort(arr ,0, arr.length - 1);
+		MyBubbleSort sort=new MyBubbleSort();
+		sort.sort( arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 	}
 
+	public void sort(int inputArr[]) {
+     //   this.arr = inputArr;
+        int length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+ 
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+         
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the arr
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the arr
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+ 
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+ 
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = arr[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                arr[k] = tempMergArr[i];
+                i++;
+            } else {
+                arr[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            arr[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+ 
+    }
+	
+	
 	/** Quick sort function **/
 	public static void quickSort(int arr[], int low, int high) {
 		int i = low, j = high;
